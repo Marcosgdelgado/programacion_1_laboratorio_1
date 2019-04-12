82b9e95dc8c93edb10e5fc8f07829bee37f871c4
Marcos Delgado <marcosgdelgado@outlook.com>
2019-04-11 22:20:21 -0300

    clase 8

--- Clase7/main.c
+++ Clase7/main.c
@@ -1,38 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Libreria.h"
-
-//int getString(char *resultado, char *msg, char *msgError,int minimo,int maximo, int reintentos);
-//***********************************************************************************************
-//int getNumber(int *resultado, char *msg, char *msgError,int minimo,int maximo, int reintentos);
-//***********************************************************************************************
-//int isValidNumber(char* cadena);
-
+#include "utn.h"
+#define LEN_LISTA    100
 int main()
 {
-    int numero;
+    /*
+    strncpy(nombres[0],"toto",20);
+    strncpy(nombres[1],"pepito",20);
+    printf("nombre:%s\n",nombres[0]);
+    printf("nombre:%s\n",nombres[1]);
+    */
 
-    getNumber(numero,"Ingrese un numero","Error",1,10,1);
-    printf("%d", numero);
+    /*
+    strncpy(nombres[0],"\0",20);
+    nombres[0][0] = '\0';
+    nombres[1][0] = '\0';
+    nombres[2][0] = '\0';
+    */
+    int opcion;
+    char nombres[LEN_LISTA][20];
+    int i;
+    int posLibre;
+    char buffer[20];
+    int posicionaeliminar;
+
+    for(i=0; i<LEN_LISTA; i++)
+        nombres[i][0] = '\0';
+
+    while(1)
+    {
+
+        utn_getNumber(&opcion,
+                      "1)ingresar\n2)listar\n4)borrar\n",
+                      "NO!",
+                      1,10,2);
+
+        printf("elegiste:%d\n",opcion);
+        switch(opcion)
+        {
+            case 1:
+            {
+                printf("agregar nombre\n");
+                if(buscarlibre(nombres,LEN_LISTA,&posLibre)==0)
+                {
+                    printf("1.se encontro lugar en %d\n",posLibre);
+                    //utn_getString(buffer,"ingrese:","no!",0,20,1);
+                    fgets(buffer,20,stdin);
+                        printf("se ingreso:%s",buffer);
+                        //lo que no va ->nombres[posLibre]=buffer;
+                        strncpy(nombres[posLibre],buffer,20);
+                        //strncpy(nombres[1],"HOL",20);
+                        //strncpy(nombres[2],"OLA",20);
+                        for (i=0;i<10;i++)
+                            printf("%d: %s\n",i,nombres[i]);
+                }
+                break;
+            }
+            case 4:
+            {
+                fgets(buffer,20,stdin);buffer[strlen(buffer)];
+
+                buscanombre(buffer,nombres,LEN_LISTA,&posicionaeliminar);
+
+            nombres[posicionaeliminar][0]='\0';
+            break;
+            }
+        }
+
+     }
     return 0;
 }
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main () {
-   int val;
-   char str[20];
-
-   strcpy(str, "98993489");
-   val = atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
-   strcpy(str, "tutorialspoint.com");
-   val = atoi(str);
-   printf("String value = %s, Int value = %d\n", str, val);
-
-   return(0);
-}*/
---
+++ Clase7/utn.h
@@ -0,0 +1,26 @@
+#ifndef UTN_H_INCLUDED
+#define UTN_H_INCLUDED
+
+
+
+//a partir de esta funcion puedo crear muchas otras de validacion
+int utn_isValidIntNumber(char* pCadena);
+
+int utn_getNumber  (int *pResultado,
+                    char *msj,
+                    char *msjError,
+                    int minimo,
+                    int maximo,
+                    int reintentos);
+
+int utn_getString  (char *pResultado,
+                    char *msj,
+                    char *msjError,
+                    int minimo,
+                    int maximo,
+                    int reintentos);
+
+int buscarlibre(char lista[][20], int len,int* pIndex);
+int buscanombre(char* pNombre,char lista[][20],int len, int* pIndex);
+#endif // UTN_H_INCLUDED
+
